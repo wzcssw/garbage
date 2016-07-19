b16c82499db91ee9a3aa7134b7d9674d9a60a9ee
王志成 <wangzhicheng@tongxinyiliao.com>
2016-07-19 18:06:13 +0800

    modify index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'welcome/index'
 
   mount API::Base => '/api'
   # Example of regular route:
