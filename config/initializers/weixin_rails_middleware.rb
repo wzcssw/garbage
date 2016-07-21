8cdb69fd453838f3ce0b61851962598506b7cf0d
王志成 <wangzhicheng@tongxinyiliao.com>
2016-07-21 14:45:16 +0800

    add scret

--- config/initializers/weixin_rails_middleware.rb
+++ config/initializers/weixin_rails_middleware.rb
@@ -9,8 +9,8 @@ WeixinRailsMiddleware.configure do |config|
   # using to weixin server url to validate the token can be trusted.
   config.weixin_secret_string = 'Sdzfz9Ux-8eccbd9d4379fb63fe8b722f84e9720c'
   # 加密配置，如果需要加密，配置以下参数
-  # config.encoding_aes_key = 'rKxZA4EcSTmAhbszL3uxS3lnbFo0RbvSlGEMpAYoxaO'
-  # config.app_id = "wx166591b059004da0"
+  config.encoding_aes_key = 'rKxZA4EcSTmAhbszL3uxS3lnbFo0RbvSlGEMpAYoxaO'
+  config.app_id = "wx166591b059004da0"
 
   ## You can custom your adapter to validate your weixin account ##
   # Wiki https://github.com/lanrion/weixin_rails_middleware/wiki/Custom-Adapter
