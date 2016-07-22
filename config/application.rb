e0e0b09e52b2d610e7e4f3de8ba5780820174106
王志成 <wangzhicheng@tongxinyiliao.com>
2016-07-22 14:37:24 +0800

    sd

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module Garbage
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
